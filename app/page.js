c6c1a69889b1157cd1430509aac5898166f6f2bd
AbhishekChoudhari42 <choudhariabhishek93@gmail.com>
2023-09-23 14:01:53 +0530

    next ui added

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import Image from 'next/image'
-
+import { Button } from '@nextui-org/button'
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    
+    <main className="p-8">
+  
+      <div className='w-full bg-neutral-900 h-12 rounded-lg'>
+         
+      </div>      
     </main>
   )
 }
+
